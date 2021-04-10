d9e426cb766034155ec3026517ee2555048c208b
Matheus Nunes <matheusnunesmiguel6@gmail.com>
2021-04-09 23:24:58 -0300

    Adicionando novamente o objeto Mask e validando o CEP

--- atividadeExpressoesRegulares/scripts/maskValidators.js
+++ atividadeExpressoesRegulares/scripts/maskValidators.js
@@ -1,3 +1,29 @@
+const Mask= {
+    cpfCnpj(input){
+        //Pegando o conteúdo do input
+        let value = input.value;
+            //Trocando letras por espaços vazios
+            value = value.replace(/\D/,"");
+            //apagando caracteres(numeros) com tamanho maior que 14
+            if(value.length > 14){
+                value = value.slice(0,-1);
+            }
+        //montando o formato do cnpj 11.111.111/1111-11
+        value = value.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5");
+        input.value = value;
+    },
+    cep(input){
+        let value = input.value;
+            value = value.replace(/\D/,"");
+            if(value.length > 8){
+                value = value.slice(0,-1);
+            }
+            value = value.replace(/(\d{5})(\d{3})/, "$1-$2");
+            input.value = value;
+    }
+}
+
+
 const Validate = {
     isEmail(input){
         //apaga msg de erro, se tiver
@@ -23,6 +49,18 @@ const Validate = {
         const cleanValues = value.replace(/\D/g, "");
             if(cleanValues.length <14 && cleanValues.length != 14){
                 error = 'CNPJ inválido';
+                Validate.displayError(input,error);
+                return;
+            }
+            input.value = value;
+    },
+    isCEP(input){
+        Validate.clearErrors(input);
+        let value = input.value;
+        let error = null;
+        const cleanValues = value.replace(/\D/g,"");
+            if(cleanValues.length != 8){
+                error = 'CEP inválido';
                 Validate.displayError(input,error);
                 return;
             }
